6824c0ab4be60c5ede8c798f4984ccfdf9854711
Patrick Kuntschnik <patrick.kuntschnik@gmail.com>
2015-03-22 15:16:23 +0000

    update

---
+++ js/s.js
@@ -0,0 +1,7 @@
+$(function() {
+    //$(window).resize(function() {
+    //    $("body").css("width",$(window).width()*0.80);
+    //});
+    //$("body").css("width",$(window).width()*0.80);
+    $("body").flowtype({fontRatio:35});
+});
