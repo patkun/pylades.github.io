ef6347bf4847c4faa293b89211cc56b633c55dde
Patrick Kuntschnik <patrick.kuntschnik@gmail.com>
2015-03-23 15:58:25 +0000

    update

--- js/s.js
+++ js/s.js
@@ -3,5 +3,5 @@ $(function() {
     //    $("body").css("width",$(window).width()*0.80);
     //});
     //$("body").css("width",$(window).width()*0.80);
-    $("body").flowtype({fontRatio:35});
+    $("body").flowtype({fontRatio:35,minFont:16});
 });
