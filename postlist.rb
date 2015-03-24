de74d88bb7dff813ca91045e501ea35649834214
Patrick Kuntschnik <patrick.kuntschnik@gmail.com>
2015-03-24 15:38:23 +0000

    update

---
+++ postlist.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+require 'yaml'
+
+
+posts = Array.new
+Dir.glob('posts/*.public.txt'){|f|
+  posts << [f[/([0-9]{4}-[0-9]{2}-[0-9]{2})/,1],YAML.load_file(f)["title"],f]
+}
+o = String.new
+posts.sort {|a,b| b[0] <=> a[0]}.each do |p|
+  o << "[#{p[1]} — #{p[0]}](#{p[2]})\n"
+end
+
+contents = File.read('index.edit.txt').sub(/~---posts---~/,o)
+
+
+File.open('index.processed.txt', 'w') { |file| file.write(contents) }
