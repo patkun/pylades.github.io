afdae9b6376cc17c890e3c6476965ae9d164208d
Patrick Kuntschnik <patrick.kuntschnik@gmail.com>
2015-03-27 10:23:03 +0000

    update

--- js/s.js
+++ js/s.js
@@ -4,4 +4,22 @@ $(function() {
     //});
     //$("body").css("width",$(window).width()*0.80);
     $("body").flowtype({fontRatio:35,minFont:20});
+    var pic_real_width, pic_real_height;
+    $("figure img").each(function() {; // Get my img elem
+                                     var img = $(this);
+                                     $("<img/>") // Make in memory copy of image to avoid css issues
+                                     .attr("src", $(this).attr("src"))
+                                     .load(function() {
+                                         pic_real_width = this.width;   // Note: $(this).width() will not
+                                         pic_real_height = this.height; // work for in memory images.
+                                         if (pic_real_width > $(window).width()) {
+                                             img.css("max-width",$(window).width());
+                                             img.parent().offset({left:0}).css("max-width",$(window).width());
+                                         }
+                                         else {
+                                             img.css("float","right");
+                                         }
+                                     });
+                                    });
+    $("figcaption").css({"font-family":"sans-serif","font-size":"50%"});
 });
